2895eed7b68e2a40c021f8432703f582a5cc699c
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2023-01-19 19:39:53 -0500

    first commit

---
+++ Examples.java
@@ -0,0 +1,13 @@
+public class Examples {
+    public static void main(String[] args) {
+        System.out.println("this class sucks");
+
+        // void type has no returned value
+        // void type is used for methods that do not return a value
+
+        // what are java types?
+        // int, double, boolean, String, char, void
+        String dog = "Jim";
+        System.out.println(dog);
+    }
+}

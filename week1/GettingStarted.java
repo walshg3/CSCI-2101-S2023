80a5f11085b51ec5e7b389ca4860166961cce3e3
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2023-01-26 18:46:22 -0500

    first java program commit

--- week1/GettingStarted.java
+++ week1/GettingStarted.java
@@ -4,5 +4,6 @@ public class GettingStarted {
     // Stinky Cheese
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is our fierst Java program!");
     }
 }

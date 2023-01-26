b4108bc0d6b6a553deb2ac024b1d05e0d6e17fe6
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2023-01-26 18:47:33 -0500

    typo

--- week1/GettingStarted.java
+++ week1/GettingStarted.java
@@ -4,6 +4,6 @@ public class GettingStarted {
     // Stinky Cheese
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("This is our fierst Java program!");
+        System.out.println("This is our first Java program!");
     }
 }

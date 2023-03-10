4795852a14b7a824f892858d29d8f7806bc28dde
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2023-03-09 19:18:05 -0500

    started Bank Account

---
+++ week5/BankAccount.java
@@ -0,0 +1,169 @@
+package week5;
+    /*
+     * Appas Bank
+     * Bank Account Parameters:
+     * public Name -> String ✓
+     * private Balance -> double ✓ 
+     * private Date of Birth -> String ✓
+     * private SSN -> int (Maybe String) ✓
+     * private Account Number -> int ✓
+     * 
+     * Bank Account Methods:
+     * Constructor 
+     * Getters and Setters
+     * Deposit
+     * Withdraw
+     * Transfer (HomeWork)
+     * Balance
+     * Menu
+     * Compound Interest
+     * Simple Interest (HomeWork)
+     * Authentication Basic
+     * Authentication Advanced (HomeWork?)
+     * 
+     * Bank Account Constructor
+     * Name, Balance, SSN, Account Number, Date of Birth
+     * Name, SSN
+     */
+
+public class BankAccount {
+
+    // Bank Account Parameters
+    public String name;
+    private double balance;
+    private String dob;
+    private int ssn;
+    private int accountNumber;
+
+
+
+
+    // Bank Account Constructor
+    public BankAccount(String name, int ssn, double balance, int accountNumber, String dob) {
+        this.name = name;
+        this.ssn = ssn;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.dob = dob;
+    }
+
+    // Bank Account Constructor
+    public BankAccount(String name, int ssn) {
+        this.name = name;
+        this.ssn = ssn;
+    }
+
+
+    // Bank Account Methods
+
+    // Note you do need to create a getter for public variables however it is best practice to create a getter for all variables
+
+    /*
+     * Getters and Setters
+     */
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getbalance() {
+        return this.balance;
+    }
+
+    public String getdob() {
+        return this.dob;
+    }
+
+    public int getssn() {
+        return this.ssn;
+    }
+
+    public int getaccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    /*
+     * Should only be used by during the creation of the account never after instead see deposit and withdraw
+     */
+    public void setbalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setdob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setssn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setaccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
+
+
+
+
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void transfer(double amount, BankAccount other) {
+        this.withdraw(amount);
+        other.deposit(amount);
+    }
+
+
+
+    public void menu() {
+        System.out.println("Welcome to Appas Bank");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Transfer");
+        System.out.println("4. Balance");
+        System.out.println("5. Compound Interest");
+        System.out.println("6. Simple Interest");
+        System.out.println("7. Exit");
+    }
+
+    public void compoundInterest(double rate, int years) {
+        double interest = this.balance * Math.pow((1 + rate), years);
+        this.balance += interest;
+    }
+
+    public void simpleInterest(double rate, int years) {
+        double interest = this.balance * rate * years;
+        this.balance += interest;
+    }
+
+}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+
+//         BankAccount myAccount = new BankAccount("Appa", 1000, 123456789, 123456789, "01/01/2000");
+//         BankAccount yourAccount = new BankAccount("Momo", 123456789);
+
+//         myAccount.deposit(100);
+//         myAccount.withdraw(50);
+//         myAccount.transfer(100, yourAccount);
+//         System.out.println(myAccount.balance());
+//         System.out.println(yourAccount.balance());
+//         myAccount.menu();
+//         myAccount.compoundInterest(0.05, 5);
+
+
+
+
+
+
+// }
---
+++ week5/BankAccountTester.java
@@ -0,0 +1,42 @@
+package week5;
+
+public class BankAccountTester {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        BankAccount myAccount = new BankAccount("MOMO", 8675309);
+
+        System.out.println(myAccount.name);
+
+        BankAccount yourAccount = new BankAccount("Katara", 1234567, 1000, 101, "01/01/2000");
+
+        System.out.println(yourAccount.name);
+
+        System.out.println(yourAccount.getbalance());
+        System.out.println(yourAccount.getdob());
+        System.out.println(yourAccount.getssn());
+        System.out.println(yourAccount.getaccountNumber());
+
+        System.out.println("===========");
+
+        System.out.println(myAccount.getbalance());
+        System.out.println(myAccount.getdob());
+        System.out.println(myAccount.getssn());
+        System.out.println(myAccount.getaccountNumber());
+
+        System.out.println("===========");
+        myAccount.setdob("01/02/2000");
+        System.out.println(myAccount.getdob());
+        myAccount.setaccountNumber(102);
+        System.out.println(myAccount.getaccountNumber());
+        System.out.println("===========");
+        System.out.println(myAccount.getbalance());
+        // myAccount.balance = 1000000; // This does not work because balance is private
+        myAccount.deposit(100);
+        System.out.println(myAccount.getbalance());
+        
+        yourAccount.deposit(100);
+        System.out.println(yourAccount.getbalance());
+
+    }
+}

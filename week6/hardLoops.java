01938908d0f4f58a64cb8231b19a2d5f19347e4c
walshg3 <walshg3@go.stockton.edu>
2023-04-11 22:01:09 -0400

    loop work

--- week6/hardLoops.java
+++ week6/hardLoops.java
@@ -32,12 +32,79 @@ public class hardLoops {
         return result;
     }
 
+    // Create a method that will check if a string is a palindrome
+    // A palindrome is a word that is spelled the same forwards and backwards
+    // Ex.
+    // isPalindrome("racecar") → true
+    // isPalindrome("hello") → false
+    // isPalindrome("level") → true
+    public static boolean isPalindrome(String str) {
+        boolean result = true;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) { // this is an example of using a pointer both forward and backwards
+                result = false;
+            }
+        }
+        return result;
+    }
+
+
+    // Create the palindrome method using a while loop
+    public static boolean isPalindromeWhile(String str) {
+        boolean result = true;
+        int i = 0; // pointer that will start at the beginning of the string
+        int j = str.length() - 1; // pointer that will start at the end of the string
+        while (i < j / 2) {
+            if (str.charAt(i) != str.charAt(j - i)) {
+                result = false;
+            }
+            i++;
+            j--;
+        }
+        return result;
+    }
+
+    // Create a method that will count the number of times 'xx' appears in a string then return the number of times it appears
+    // Ex.
+    // countXX("abcxx") → 1
+    // countXX("xxx") → 2
+    // countXX("xxxx") → 3
+    public static int countXX(String str) {
+        int result = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x') { // there is 2 points the pointer is at so we need to check both
+                // i = 0, i + 1 = 1
+                result++;
+            }
+        }
+        return result;
+    }
+
+    // Create the same method but use substring and equalsignorecase
+    public static int countXX2(String str) {
+        int result = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equalsIgnoreCase("xx")) { // this is an example of overloaded methods
+                result++;
+            }
+        }
+        return result;
+    }
+
+
+
+
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println(everyNth("Miracle", 2));
-        System.out.println(oneTwo("tcagdo"));
+        // System.out.println("Hello World");
+        // System.out.println(everyNth("Miracle", 2));
+        // System.out.println(oneTwo("tcagdo"));
+        // System.out.println(isPalindrome("racecar"));
+        // System.out.println(isPalindromeWhile("racecar"));
+
+        System.out.println(countXX("axxbxxcxx"));
+
 
 
     }
